88202a1b67cee368f82306717291196437fee7a5
Saint-Jean Germila <germila@noukod.com>
2019-03-04 16:44:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function distanceFromHqInBlocks (distance) {
 }
 
 function distanceFromHqInFeet (distance) {
-  return distanceFromHqInBlocks(distance) *42
+  return distanceFromHqInBlocks(distance) *264
 }
 
 function distanceTravelledInFeet (uptown, downtown) {
-  return Math.abs(downtown - uptown)  * 42
+  return Math.abs(downtown - uptown)  * 264
 }
 
 function calculatesFarePrice(start, destination) {
